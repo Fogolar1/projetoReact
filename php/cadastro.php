f1762a5e9e9ee1007dcb633a8e1af64b3afc1111
enzo.fogolari <enzo.fogolari@neomind.com.br>
2022-11-11 08:39:32 -0300

    v2

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require 'connection/connection.php';
 
 header('Access-Control-Allow-Origin: http://localhost:3001');
@@ -7,5 +7,22 @@ header('Access-Control-Allow-Origin: http://localhost:3001');
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-echo ("me mama");
+$query = $db->prepare("SELECT * FROM Usuarios where nomeUsuario = ?");
+$query->bind_param("s", $usuario);
+
+$query->execute();
+$resultado = $query->get_result();
+
+if($resultado->num_rows > 0){
+    while($row = $resultado->fetch_assoc()) {
+        if($row["senhaUsuario"] == $senha){
+            echo("login");
+            $db->close();
+            return;
+        }
+      }
+}
+
+echo("teste");
+$db->close();
 ?>
